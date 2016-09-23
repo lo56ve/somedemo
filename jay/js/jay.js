713e61a9d28f4fb7453e2da0b0574911627d6350
赵晓栋 <1046866318@qq.com>
2016-09-23 10:59:30 +0800

    增加一些曲目

--- jay/js/jay.js
+++ jay/js/jay.js
@@ -34,7 +34,7 @@ window.onload=function(){
 	carousel.style.height=clientH+"px";
 	ul.style.height=clientH+"px";
 	ul.style.width=clientW*6+"px";
-	lyricUl.style.height=lyricLi[0].offsetHeight*40+"px";
+	// lyricUl.style.height=lyricLi.offsetHeight*40+"px";
 	special.style.height=clientH+"px";
 	special.style.width=clientW+"px";
 	for(var i=0;i<carouselImg.length;i++){
@@ -79,7 +79,6 @@ window.onload=function(){
 	function handle(delta){
 		if(delta<0){			//向下滚动
 			clearInterval(scrollTimer);
-			console.log(inner.offsetHeight);
 			if(Math.abs(inner.offsetTop)<inner.offsetHeight-clientH){
 				scrollTimer=setInterval(function(){
 					inner.style.top=inner.offsetTop-1+"px";
@@ -165,28 +164,6 @@ window.onload=function(){
 		}
 	}
 
-	//歌词滚动事件
-	var lyricScrollTimer=null;
-	lyric.onclick=function(){
-		// clearInterval(lyricScrollTimer);
-		// lyricScrollTimer=setInterval(function(){
-		// 	lyricUl.style.top=lyricUl.offsetTop-1+"px";
-		// 	if(lyricUl.offsetTop==lyricUl.offsetHeight){
-		// 		clearInterval(lyricScrollTImer);
-		// 	}
-		// 	for(var i=0;i<lyricLi.length;i++){
-		// 		if((lyricLi[i].offsetTop+lyricUl.offsetTop<250) &&(lyricLi[i].offsetTop+lyricUl.offsetTop>210)){
-		// 			lyricLi[i].className="scrollLi";
-		// 		}
-		// 		else{
-		// 			lyricLi[i].className="";
-		// 		}
-		// 	}
-		//  },30)
-		music.pause();
-		music.currentTime=0;
-	}
-
 
 	//异步加载专辑的图片和介绍
 	specialChange();
@@ -249,18 +226,150 @@ window.onload=function(){
 							}
 						});
 					}
-					if(obj==scrollSingleA && i==0){		//播放音乐
-						music.play();
-					}
-					else if(i!=0){
+					if(obj==scrollSingleA){		//播放音乐
 						music.pause();
 						music.currentTime=0;
+						lyricUl.innerHTML="";
+						music.innerHTML="";
+						var lrcURL;
+						console.log(i);
+						switch(i){
+							case 0:{
+								music.innerHTML="<source src='mp3/告白气球/告白气球.mp3' type='audio/mp3'><source src='mp3/告白气球/告白气球.ogg' type='audio/ogg'>";
+								lrcURL="mp3/告白气球/告白气球";
+							}
+							break;
+							case 1:{
+								music.innerHTML="<source src='mp3/晴天/晴天.mp3' type='audio/mp3'><source src='mp3/晴天/晴天.ogg' type='audio/ogg'>";
+								lrcURL="mp3/晴天/晴天";
+							}
+							break;
+							case 2:{
+								music.innerHTML="<source src='mp3/爱情废柴/爱情废柴.mp3' type='audio/mp3'><source src='mp3/爱情废柴/爱情废柴.ogg' type='audio/ogg'>";
+								lrcURL="mp3/爱情废柴/爱情废柴";
+							}
+							break;
+							case 3:{
+								music.innerHTML="<source src='mp3/彩虹/彩虹.mp3' type='audio/mp3'><source src='mp3/彩虹/彩虹.ogg' type='audio/ogg'>";
+								lrcURL="mp3/彩虹/彩虹";
+							}
+							break;
+							case 4:{
+								music.innerHTML="<source src='mp3/七里香/七里香.mp3' type='audio/mp3'><source src='mp3/七里香/七里香.ogg' type='audio/ogg'>";
+								lrcURL="mp3/七里香/七里香";
+							}
+							break;
+							case 5:{
+								music.innerHTML="<source src='mp3/一路向北/一路向北.mp3' type='audio/mp3'><source src='mp3/一路向北/一路向北.ogg' type='audio/ogg'>";
+								lrcURL="mp3/一路向北/一路向北";
+							}
+							break;
+							case 6:{
+								music.innerHTML="<source src='mp3/稻香/稻香.mp3' type='audio/mp3'><source src='mp3/稻香/稻香.ogg' type='audio/ogg'>";
+								lrcURL="mp3/稻香/稻香";
+							}
+							break;
+							case 7:{
+								music.innerHTML="<source src='mp3/不能说的秘密/不能说的秘密.mp3' type='audio/mp3'><source src='mp3/不能说的秘密/不能说的秘密.ogg' type='audio/ogg'>";
+								lrcURL="mp3/不能说的秘密/不能说的秘密";
+							}
+							break;
+						}
+						music.play();
+						$.ajax({
+							url:lrcURL+'.lrc',
+							dataType:'text',
+							async:'true',
+							type:'GET',
+							scriptCharset:'utf-8',
+							success:function(data){
+								var a= parseLyric(data);
+								//获取页面上的audio标签
+								var last=-1;
+								for(var i=0,l=a.length;i<l;i++){
+									lyricUl.innerHTML+="<li>"+a[i][1]+"</li>";
+								}
+								music.ontimeupdate=function(e){
+									if(!lyricUl.innerHTML){
+										return;
+									}
+									for(var i=0,l=a.length;i<l;i++){
+										if(this.currentTime/*当前播放时间*/>a[i][0]){
+											if(i>last){
+												console.log(this.currentTime);
+												lyricScroll(a);
+												last=i;
+											}
+										}
+									}
+								}
+
+							}
+						})
 					}
 				}
-			})(i)
+			})(i)		//这里的i表示第几首歌
 		}
 	}
 	
+
+	//歌词滚动事件
+	function lyricScroll(li){
+		var lyricScrollTimer=null;
+		clearInterval(lyricScrollTimer);
+		lyricScrollTimer=setInterval(function(){
+			lyricUl.style.top=lyricUl.offsetTop-1+"px";
+			if(lyricUl.offsetTop==lyricUl.offsetHeight){
+				clearInterval(lyricScrollTimer);
+			}
+			else if(lyricUl.offsetTop%41==0){
+				clearInterval(lyricScrollTimer);
+			}
+			for(var i=0;i<li.length;i++){
+				if((lyricLi[i].offsetTop+lyricUl.offsetTop<250) &&(lyricLi[i].offsetTop+lyricUl.offsetTop>210)){
+					lyricLi[i].className="scrollLi";
+				}
+				else{
+					lyricLi[i].className="";
+				}
+			}
+		 },3)
+	}
+
+	//提取歌词
+	function parseLyric(text) {
+	    //将文本分隔成一行一行，存入数组
+	    var lines = text.split('\n'),
+	        //用于匹配时间的正则表达式，匹配的结果类似[xx:xx.xx]
+	        pattern = /\[\d{2}:\d{2}.\d{2}\]/g,
+	        //保存最终结果的数组
+	        result = [];
+	    //去掉不含时间的行
+	    while (!pattern.test(lines[0])) {
+	        lines = lines.slice(1);
+	    };
+	    //上面用'\n'生成生成数组时，结果中最后一个为空元素，这里将去掉
+	    lines[lines.length - 1].length === 0 && lines.pop();
+	    lines.forEach(function(v /*数组元素值*/ , i /*元素索引*/ , a /*数组本身*/ ) {
+	        //提取出时间[xx:xx.xx]
+	        var time = v.match(pattern),
+	            //提取歌词
+	            value = v.replace(pattern, '');
+	        //因为一行里面可能有多个时间，所以time有可能是[xx:xx.xx][xx:xx.xx][xx:xx.xx]的形式，需要进一步分隔
+	        time.forEach(function(v1, i1, a1) {
+	            //去掉时间里的中括号得到xx:xx.xx
+	            var t = v1.slice(1, -1).split(':');
+	            //将结果压入最终数组
+	            result.push([parseInt(t[0], 10) * 60 + parseFloat(t[1]), value]);
+	        });
+	    });
+	    //最后将结果数组中的元素按时间大小排序，以便保存之后正常显示歌词
+	    result.sort(function(a, b) {
+	        return a[0] - b[0];
+	    });
+	    return result;
+	}
+
 	//获取样式
 	function getClass(obj,name){
 		if(obj.currentStyle){
